b82ec8094922bb3fec6c6e97f7043b6e74613435
Gulshan Varma <varmagulshan11@gmail.com>
2022-07-04 20:42:49 +0530

    ok

---
+++ automata.ps1
@@ -0,0 +1,16 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName(“System.Diagnostics”)
+
+$sw = new-object System.Diagnostics.Stopwatch
+$sw.Start()
+
+while ($sw.elapsed.Milliseconds –lt 10) {$null}; 
+& 'C:\Program Files\VideoLAN\VLC\vlc.exe' --qt-start-minimized --play-and-exit --qt-notification=0 "C:\Users\shu\Downloads\Start_alarm.mp3"
+
+& start devenv
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
+
+while ($sw.elapsed.Minutes –lt 40) {$null}; 
+powershell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('Automatic logoff after 1 hour of inactivity','WARNING')}"
+& 'C:\Program Files\VideoLAN\VLC\vlc.exe' --qt-start-minimized --play-and-exit --qt-notification=0 "C:\Users\shu\Downloads\Start_alarm.mp3"
+
+$sw.Stop()
---
+++ automate_startup.cmd
@@ -0,0 +1,1 @@
+powershell.exe -windowstyle hidden -file C:\Users\shu\Desktop\automate_startup\automate-boot\automata.ps1 >> ./automata_logs.txt
