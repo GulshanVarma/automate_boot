1a720e21a4af3de6de419a53fbf7ecdb66600ff0
Gulshan Varma <varmagulshan11@gmail.com>
2022-07-04 21:58:54 +0530

    ok, 40 min

--- automata.ps1
+++ automata.ps1
@@ -6,15 +6,15 @@ $sw = new-object System.Diagnostics.Stopwatch
 $sw.Start()
 Write-Host "stopwatch started for 40 minutes"
 
-while ($sw.elapsed.Minutes –lt 40) {$null}; 
-& 'C:\Program Files\VideoLAN\VLC\vlc.exe' --qt-start-minimized --play-and-exit --qt-notification=0 "C:\Users\shu\Downloads\Start_alarm.mp3"
 while ($sw.elapsed.Milliseconds –lt 1) {$null}; 
+& 'C:\Program Files\VideoLAN\VLC\vlc.exe' --qt-start-minimized --play-and-exit --qt-notification=0 "C:\Users\shu\Downloads\Start_alarm.mp3"
+ 
 [reflection.assembly]::loadwithpartialname('System.Windows.Forms')
 [reflection.assembly]::loadwithpartialname('System.Drawing')
 $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
-$notify.showballoontip(10,'WARNING','Timer started 40 Minutes ! Enjoy !!',[system.windows.forms.tooltipicon]::None)
+$notify.showballoontip(20,'WARNING','Timer started 40 Minutes ! Enjoy !!',[system.windows.forms.tooltipicon]::None)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -25,8 +25,8 @@ Start-Process -FilePath "C:\Program Files (x86)\Microsoft SQL Server Management 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Write-Host "Programs started successfully"
-while ($sw.elapsed.Seconds –lt 10) {$null}; 
-$notify.showballoontip(10,'WARNING','You did **Great**, now Enjoy',[system.windows.forms.tooltipicon]::None)
+while ($sw.elapsed.Minutes –lt 40) {$null}; 
+$notify.showballoontip(20,'WARNING','You did **Great**, now Enjoy',[system.windows.forms.tooltipicon]::None)
 & 'C:\Program Files\VideoLAN\VLC\vlc.exe' --qt-start-minimized --play-and-exit --qt-notification=0 "C:\Users\shu\Downloads\Start_alarm.mp3"
 
 $sw.Stop()
